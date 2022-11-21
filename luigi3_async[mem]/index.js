92d37fe2cbdf2bcf84bdd87c7636b5f292e038b6
x87 <x87@users.noreply.github.com>
2022-11-21 12:41:31 -0500

    simplify starter

--- luigi3_async[mem]/index.js
+++ luigi3_async[mem]/index.js
@@ -11,18 +11,10 @@ const p = new Player(0);
 const startPos = [p.getCoordinates().x + 2, p.getCoordinates().y, p.getCoordinates().z];
 // const startPos = [892.8, -425.8, 13.9];
 
+// mission starter
+// continuously check if player is in the marker and can start the mission
+// if conditions are met, start the mission
 (async () => {
-  while (true) {
-    await starter(); // starter resolves when player is in the marker and can start the mission
-    if (await startMission("./mission.mjs")) {
-      // mission completed, create a starter for the next mission
-
-      break;
-    }
-  }
-})();
-
-async function starter() {
   while (true) {
     await asyncWait(0);
     if (readScmVariable(flag_luigi_mission3_passed) || readScmVariable(flag_industrial_passed)) {
@@ -30,16 +22,18 @@ async function starter() {
     }
 
     // check if player is in the marker and can start the mission
-    if (p.isPlaying() && p.locateOnFoot3D(...startPos, 1.5, 2.0, 2.0, true) && !ONMISSION) {
-      if (p.canStartMission()) {
-        log("*** STARTING MISSION luigi3 ***");
-        p.makeSafeForCutscene();
-        Camera.SetFadingColor(0, 0, 0);
-        Camera.DoFade(1500, Fade.Out);
-        Streaming.Switch(false);
-        Text.PrintBig("LM3", 15000, 2); //"Drive Misty For Me."
-        return fading();
+    if (p.isPlaying() && p.locateOnFoot3D(...startPos, 1.5, 2.0, 2.0, true) && !ONMISSION && p.canStartMission()) {
+      log("*** STARTING MISSION luigi3 ***");
+      p.makeSafeForCutscene();
+      Camera.SetFadingColor(0, 0, 0);
+      Camera.DoFade(1500, Fade.Out);
+      Streaming.Switch(false);
+      Text.PrintBig("LM3", 15000, 2); //"Drive Misty For Me."
+      await fading();
+      if (await startMission("./mission.mjs")) {
+        // at this point the mission is completed
+        break;
       }
     }
   }
-}
+})();
